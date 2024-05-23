3d641fc7d77c64d8436134f33cdc1fbaf5d7bc72
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-05-23 15:09:10 +0300

    1.Prototip

---
+++ Prototip 1 .py
@@ -0,0 +1,28 @@
+#Bu prototip başarısızlıkla sonuçlanmıştır...
+#FurOfTheWeak | Furkan Gül
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from getUser import username,password
+import time
+
+class Instagram():
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox()
+
+    def kullaniciGiris(self):
+        self.driver.get('https://www.instagram.com/accounts/login/')
+        time.sleep(7)
+        self.driver.find_element(By.NAME, 'username')
+        self.driver.find_element(By.NAME, 'password')
+        username.send_keys(self.username)
+        password.send_keys(self.password)
+        time.sleep(7)
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+instgrm = Instagram(username,password)
+instgrm.kullaniciGiris()
